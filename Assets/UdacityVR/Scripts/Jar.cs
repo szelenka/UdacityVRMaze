ed386f0087f5d8f1c6a5534c44ec75b9a7489d6a
Scott Zelenka <szelenka@cisco.com>
2018-06-22 16:03:58 -0400

    completing maze, adjusting jar prefab

--- Assets/UdacityVR/Scripts/Jar.cs
+++ Assets/UdacityVR/Scripts/Jar.cs
@@ -12,6 +12,12 @@ public class Jar : MonoBehaviour {
     [Tooltip("Audio files to play when Jar is opened")]
     AudioClip[] openingAudioFile;
 
+    [SerializeField]
+    [Tooltip("How fast to rotate this object")]
+    private float rotationSpeed = 250.0f;
+
+    float totalRotation = 0f;
+
     AudioSource audioSource;
     bool isOpening = false;
 
@@ -35,15 +41,22 @@ public class Jar : MonoBehaviour {
         {
             return;
         }
+        totalRotation += Time.deltaTime * rotationSpeed;
+        gameObject.transform.Rotate(totalRotation, 0, 0, Space.World);
+        if (totalRotation >= 60f) 
+        {
+            BreakObject();
+        }
         //TODO: throw up in air and fall to ground to open
     }
 
     public void OnClick()
     {
+        Debug.Log("Jar::OnClick");
         isOpening = true;
-        GameObject poof = Instantiate(hiddenObject, gameObject.transform) as GameObject;
-        PlayAudioHapticFeedback();
-        Destroy(gameObject, 0.5f);
+        GameObject hidden = Instantiate(hiddenObject, gameObject.transform, true) as GameObject;
+        hidden.SetActive(true);
+        BreakObject();
     }
 
     private void PlayAudioHapticFeedback()
@@ -53,4 +66,11 @@ public class Jar : MonoBehaviour {
             audioSource.Play();
         }
     } 
+
+    private void BreakObject()
+    {
+        // explode mesh and destroy
+        PlayAudioHapticFeedback();
+        Destroy(gameObject, 0.5f);
+    }
 }
